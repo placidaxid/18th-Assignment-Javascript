fe3f694fa9f833d118025a9f0db3d5d5acdc6b95
placidaxid <nwanagaplacid@gmail.com>
2022-04-14 08:36:11 -0700

    added some styles

---
+++ pla.js
@@ -0,0 +1,21 @@
+        //addition
+        function myfun()
+        {
+            var a=document.getElementById('number').value;
+            var b=document.getElementById('numbers').value;
+            document.getElementById('result').innerHTML=parseInt(a) + parseInt(b);
+        }
+        //multiplication
+        function myfun1()
+        {
+            var a=document.getElementById('number').value;
+            var b=document.getElementById('numbers').value;
+            document.getElementById('result').innerHTML=(a*b);
+        }
+        //division
+        function myfun2()
+        {
+            var a=document.getElementById('number').value;
+            var b=document.getElementById('numbers').value;
+            document.getElementById('result').innerHTML=(a/b);
+        }
